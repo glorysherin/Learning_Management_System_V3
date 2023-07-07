df4696cfce7b7a55cc939edd21edf2ea5da3f032
nagipragalathan <kk2069037@gmail.com>
2023-07-07 13:47:39 +0530

    sample

--- base/Routes/Upload_Assignment.py
+++ base/Routes/Upload_Assignment.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from base.models import Upload_Assignment, Assignment
+from base.models import Upload_Assignment, Assignment,Student, Users
+from .Tool.Tools import student_detials, staff_detials
+from django.contrib.auth.models import User
 
 def upload_assignment_list(request,id):
-    users = Upload_Assignment.objects.filter(Assignment_id=id).values('update_by').distinct()
-    return render(request, 'assignment/upload_assignment_list.html', {'assignments': users})
+    users = Upload_Assignment.objects.filter(Assignment_id=id).values('update_by')
+    sample=[]
+    for i in users:
+        print("id is....",i.get('update_by'))
+        user = User.objects.get(id=i.get('update_by'))
+        student = Student.objects.get(user=user)
+        sample.append(student)
+    print(sample)
+    return render(request, 'teacher/assignments.html', staff_detials(request,'Submited Students',{'datas': zip(users,sample)}))
+
 
 def upload_assignment_create(request,qst_id):
     obj = Assignment.objects.get(id=qst_id)
@@ -15,8 +25,8 @@ def upload_assignment_create(request,qst_id):
         file = request.FILES['file']
         upload_assignment = Upload_Assignment.objects.create(update_by=request.user.id, File=file, Assignment_id=qst_id)
         upload_assignment.save()
-        return redirect('upload_assignment_list')
-    return render(request, 'assignment/upload_assignment_create.html',{'data':obj,"file":file})
+        return redirect('upload_assignment_create',qst_id=qst_id)
+    return render(request, 'assignment/upload_assignment_create.html',{'data':obj,"file":file,"qst_id":qst_id})
 
 def upload_assignment_edit(request, pk):
     upload_assignment = get_object_or_404(Upload_Assignment, pk=pk)
